8746b564282f2085e533e3775e0787bc5ab7d4f7
dominhkhoi2002 <khoi.dominh2306@gmail.com>
2024-03-30 13:07:24 +0700

    Checkpoint

--- LeNet.py
+++ LeNet.py
@@ -1,21 +1,25 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class LeNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, 5, 1, padding=2)
-        self.bn1 = nn.BatchNorm2d(32)  # Batch normalization after the first convolutional layer
+        # Batch normalization after the first convolutional layer
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, 3, 1, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 64, 3, 1, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)  # Batch normalization after the third convolutional layer
+        # Batch normalization after the third convolutional layer
+        self.bn3 = nn.BatchNorm2d(64)
         self.conv4 = nn.Conv2d(64, 128, 3, 1, padding=1)
-        self.bn4 = nn.BatchNorm2d(128)  # Batch normalization after the third convolutional layer
+        # Batch normalization after the third convolutional layer
+        self.bn4 = nn.BatchNorm2d(128)
         self.conv5 = nn.Conv2d(128, 128, 3, 1, padding=1)
         self.bn5 = nn.BatchNorm2d(128)
         self.fc1 = nn.Linear(4 * 4 * 128, 512)
-        self.fc2 = nn.Linear(512,256)
+        self.fc2 = nn.Linear(512, 256)
         self.dropout = nn.Dropout(0.5)
         self.fc3 = nn.Linear(256, 10)
 
@@ -24,22 +28,22 @@ class LeNet(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.max_pool2d(x, 2, 2)
         x = self.dropout(x)
-        
+
         x = F.relu(self.bn3(self.conv3(x)))
         x = F.relu(self.bn4(self.conv4(x)))
         x = F.max_pool2d(x, 2, 2)
-        x = self.dropout(x) 
-        
+        x = self.dropout(x)
+
         x = F.relu(self.bn5(self.conv5(x)))
         x = F.max_pool2d(x, 2, 2)
         x = self.dropout(x)
-        
+
         x = x.view(-1, 4 * 4 * 128)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        
+
         x = F.relu(self.fc2(x))
         x = self.dropout(x)
         x = self.fc3(x)
-        
+
         return x
