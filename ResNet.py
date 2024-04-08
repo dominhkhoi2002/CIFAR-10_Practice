a99b2cc4fd986b154ce4eb8f55950f0414f6d173
dominhkhoi2002 <khoi.dominh2306@gmail.com>
2024-04-08 21:04:39 +0700

    res_net 88% letsgooooo

--- ResNet.py
+++ ResNet.py
@@ -37,7 +37,7 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
-        self.in_planes = 64
+        self.in_planes = 64   # double the number of neurons
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
@@ -47,7 +47,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.dropout = nn.Dropout(p=0.5)
-        self.fc = nn.Linear(512, num_classes)
+        self.fc1 = nn.Linear(512, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -55,6 +55,7 @@ class ResNet(nn.Module):
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
             self.in_planes = planes * block.expansion
+            layers.append(nn.Dropout(p=0.3))  # Add dropout between blocks
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -63,8 +64,8 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = nn.functional.max_pool2d(out, 4)
+        out = nn.functional.avg_pool2d(out, out.size()[2:])
         out = out.view(out.size(0), -1)
         out = self.dropout(out)
-        out = self.fc(out)
+        out = self.fc1(out)
         return out
